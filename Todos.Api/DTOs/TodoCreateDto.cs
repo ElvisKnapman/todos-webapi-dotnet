944a88e0c69f9a4d3fddadf6698d239c910438fe
elvisknapman <46550724+ElvisKnapman@users.noreply.github.com>
2024-08-02 21:29:47 -0400

    Add annotations to Create DTO and TodoModel

--- Todos.Api/DTOs/TodoCreateDto.cs
+++ Todos.Api/DTOs/TodoCreateDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Todos.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todos.Api.DTOs;
 
 public class TodoCreateDto
 {
-    public string Title { get; set; } = "";
+    [Required]
+    [StringLength(300, MinimumLength = 5, ErrorMessage = "Todo title must be between 5 and 300 characters.")]
+    public string? Title { get; set; }
     public bool IsComplete { get; set; }
 }
--- Todos.Api/Models/TodoModel.cs
+++ Todos.Api/Models/TodoModel.cs
@@ -1,8 +1,15 @@
-﻿namespace Todos.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Todos.Api.Models;
+
+[Table("Todos")]
 public class TodoModel
 {
+    [Key]
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public bool IsComplete { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastModifiedDate { get; set; }
 }
