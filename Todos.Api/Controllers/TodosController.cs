df00db30c5d53a33ae8bc812a6849c292ad374f7
elvisknapman <46550724+ElvisKnapman@users.noreply.github.com>
2024-08-02 21:31:45 -0400

    Add EF Core packages, finish Todos Controller methods
    Create ApplicationDBContext class

--- Todos.Api/Controllers/TodosController.cs
+++ Todos.Api/Controllers/TodosController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Api.Mapping;
 using Todos.Api.DTOs;
+using Todos.Api.Models;
 using Todos.Api.Services;
 
 namespace Todos.Api.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class TodosController : ControllerBase
@@ -17,15 +20,16 @@ public class TodosController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> Get()
+    public async Task<IActionResult> GetAll()
     {
-        return Ok(await _todoService.GetAsync());
+        IEnumerable<TodoModel> todos = await _todoService.GetAllAsync();
+        return Ok(todos);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(int id)
+    public async Task<IActionResult> GetById([FromRoute] int id)
     {
-        var todo = await _todoService.GetByIdAsync(id);
+        TodoModel? todo = await _todoService.GetByIdAsync(id);
 
         if (todo is null)
         {
@@ -38,16 +42,41 @@ public class TodosController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] TodoCreateDto todoToCreate)
     {
-        if (todoToCreate.Title.Trim() == "")
-        {
-            return BadRequest("Todo must have a valid title");
-        }
-
-        var todo = todoToCreate.MapToTodoModel();
+        TodoModel todo = todoToCreate.MapToTodoModel();
 
         todo = await _todoService.CreateAsync(todo);
 
         return CreatedAtAction(nameof(GetById), new { id = todo.Id }, todo);
     }
 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] TodoUpdateDto updatedTodo)
+    {
+        // Confirm id of todo being updated matches the DTO id
+        if (id != updatedTodo.Id)
+        {
+            return BadRequest();
+        }
+
+        TodoModel todo = updatedTodo.MapToTodoModel();
+
+        bool updated = await _todoService.UpdateAsync(todo);
+
+        if (!updated)
+        {
+            return NotFound();
+        }
+
+        TodoGetDto response = todo.MapToGetDto();
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var deleted = await _todoService.DeleteByIdAsync(id);
+
+        return deleted ? Ok() : NotFound();
+    }
 }
---
+++ Todos.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todos.Api.Models;
+
+namespace Todos.Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<TodoModel> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
