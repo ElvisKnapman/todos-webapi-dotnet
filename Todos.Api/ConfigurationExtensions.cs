17a7c446368ef3741333b326d2781cb04a5ceb6c
elvisknapman <46550724+ElvisKnapman@users.noreply.github.com>
2024-08-04 18:41:49 -0400

    Change namespaces to file scoped

--- Todos.Api/ConfigurationExtensions.cs
+++ Todos.Api/ConfigurationExtensions.cs
@@ -3,26 +3,25 @@ using Todos.Api.Data;
 using Todos.Api.Repositories;
 using Todos.Api.Services;
 
-namespace Todos.Api
+namespace Todos.Api;
+
+public static class ConfigurationExtensions
 {
-    public static class ConfigurationExtensions
+    public static IServiceCollection AddInjections(this IServiceCollection services)
     {
-        public static IServiceCollection AddInjections(this IServiceCollection services)
+        services.AddScoped<ITodoRepository, TodoRepository>();
+        services.AddScoped<ITodoService, TodoService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddEFCore(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
         {
-            services.AddScoped<ITodoRepository, TodoRepository>();
-            services.AddScoped<ITodoService, TodoService>();
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
 
-            return services;
-        }
-
-        public static IServiceCollection AddEFCore(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-
-            return services;
-        }
+        return services;
     }
 }
--- Todos.Api/DTOs/TodoUpdateDto.cs
+++ Todos.Api/DTOs/TodoUpdateDto.cs
@@ -1,17 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Todos.Api.DTOs
+namespace Todos.Api.DTOs;
+
+public class TodoUpdateDto
 {
-    public class TodoUpdateDto
-    {
-        [Required]
-        public int Id { get; set; }
+    [Required]
+    public int Id { get; set; }
 
-        [Required]
-        [StringLength(300, MinimumLength = 5, ErrorMessage = "Todo title must be between 5 and 300 characters.")]
-        public string? Title { get; set; }
+    [Required]
+    [StringLength(300, MinimumLength = 5, ErrorMessage = "Todo title must be between 5 and 300 characters.")]
+    public string? Title { get; set; }
 
-        [Required]
-        public bool IsComplete { get; set; }
-    }
+    [Required]
+    public bool IsComplete { get; set; }
 }
