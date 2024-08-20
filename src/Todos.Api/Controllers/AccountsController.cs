7dd7ffa48f8363ff87c97122642f327b506ec040
Elvis Knapman <46550724+ElvisKnapman@users.noreply.github.com>
2024-08-20 19:19:27 -0400

    Add login endpoint
    Create LoginDto class for login endpoint. Update Program.cs with configuration for swagger to use authorization

--- src/Todos.Api/Controllers/AccountsController.cs
+++ src/Todos.Api/Controllers/AccountsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Todos.Api.DTOs.Account;
 using Todos.Api.Models;
 using Todos.Api.Services;
@@ -12,10 +13,47 @@ public class AccountsController : ControllerBase
 {
     private readonly ITokenService _tokenService;
     private readonly UserManager<AppUser> _userManager;
-    public AccountsController(ITokenService tokenService, UserManager<AppUser> userManager)
+    private readonly SignInManager<AppUser> _signInManager;
+    public AccountsController(
+        ITokenService tokenService,
+        UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager)
     {
         _tokenService = tokenService;
         _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest("Hey teddy, this code doesn't run and return because model validation runs right after model binding and before the action method executes");
+        }
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(user => user.UserName == loginDto.Username);
+
+        if (user is null)
+        {
+            return Unauthorized("Invalid username");
+        }
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if (!result.Succeeded)
+        {
+            return Unauthorized("Invalid username or password.");
+        }
+
+        var newUser = new NewUserDto()
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user)
+        };
+
+        return Ok(newUser);
     }
 
     [HttpPost("register")]
